3892596e7635f89932aac2b88306af9af6681392
Ahmad Zaidi <azaidi4@wisc.edu>
2019-05-04 07:40:51 -0500

    fix bug

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 exports.adalConfiguration = {
   authority: 'https://login.microsoftonline.com/common',
   clientID: '199bd2e4-865b-4df0-bea1-a80f3285dc70',
-  clientSecret: 'process.env.CLIENT_SECRET',
+  clientSecret: process.env.CLIENT_SECRET,
   redirectUri: 'https://get-vultured.herokuapp.com/'
 };
 
